cdbef559aba3097bff521882a124c19296c981d1
saberahmadmd <157378864+saberahmadmd@users.noreply.github.com>
2025-06-21 16:45:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,8 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-  origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
-  credentials: true,
+  origin: process.env.CLIENT_URL,
+  credentials: true
 }));
 
 app.use(express.json())

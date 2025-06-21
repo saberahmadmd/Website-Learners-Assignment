4621a084a6a94acfea708d72c7ea43c99d3e4a84
saberahmadmd <157378864+saberahmadmd@users.noreply.github.com>
2025-06-21 12:32:39 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: https://website-learners-assignment-mnl8.onrender.com/api,
+  baseURL: "https://website-learners-assignment-mnl8.onrender.com/api",
   withCredentials: true,
 });
 
